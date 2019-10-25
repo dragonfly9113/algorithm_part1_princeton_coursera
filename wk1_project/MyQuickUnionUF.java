687d2149ebe3d680c3ce071bfddb5693f41ae4d4
William Luo <weilin.william.luo@gmail.com>
2019-10-25 16:41:20 -0700

    Start to write Percolation.java

--- wk1_project/MyQuickUnionUF.java
+++ wk1_project/MyQuickUnionUF.java
@@ -8,7 +8,7 @@ public class MyQuickUnionUF {
     private int[] id;
 
     public MyQuickUnionUF(int N) {
-        id = new int[];
+        id = new int[N];
         for (int i = 0; i < N; i++) id[i] = i;
     }
 
